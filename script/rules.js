435e981cff3b0f7f25fb4308cea82125c0be4620
Kr1tX12 <155372711+Kr1tX12@users.noreply.github.com>
2024-01-02 00:44:40 +0500

    Update rules.js

--- script/rules.js
+++ script/rules.js
@@ -1,19 +1,8 @@
 let html = '';
 let html2 = '';
 for (let i = 0; i<rules.length;i++) {
-  if (rules[i].sub) {
-    html += `
-    <div class="rules-content-part rules-content-subpart">
-      <a class="rules-content-part-link rules-content-part-sublink">${rules[i].num} ${rules[i].name}</a>
-    </div>
-   `
-  } else {
-    html += `
-    <div class="rules-content-part">
-      <a class="rules-content-part-link">${rules[i].num} ${rules[i].name}</a>
-    </div>
-   `
-  }
+  
+
   let spanClass;
   if (rules[i].num < 2) {
     spanClass = 'purple-num'
@@ -26,7 +15,7 @@ for (let i = 0; i<rules.length;i++) {
   }
   
   if (rules[i].desc && rules[i].warning && rules[i].sub) {
-    html2 += `<div class="rule">
+    html2 += `<div class="rule rule-num${i}">
       <div class="rule-name">
         <span class="${spanClass}">${rules[i].num}</span> ${rules[i].name}
       </div>
@@ -38,7 +27,7 @@ for (let i = 0; i<rules.length;i++) {
       </div>
     </div>`
   } else if (rules[i].desc && rules[i].sub) {
-    html2 += `<div class="rule">
+    html2 += `<div class="rule rule-num${i}">
       <div class="rule-name">
         <span class="${spanClass}">${rules[i].num}</span> ${rules[i].name}
       </div>
@@ -47,12 +36,45 @@ for (let i = 0; i<rules.length;i++) {
       </div>
     </div>`
   } else if (!rules[i].sub) {
-    html2 += `<div class="rule">
+    html2 += `<div class="rule rule-num${i}">
       <div class="rule-name rule-name-main">
         <span class="rule-num-main">${rules[i].num}.</span> ${rules[i].name}
       </div>
     </div>`
   }
+  
+  
+  
+  
+
 }
-document.querySelector('.rules-content').innerHTML = html;
+
 document.querySelector('.rules').innerHTML = html2;
+
+
+for (let i = 0; i<rules.length; i++) {
+  const element = document.querySelector(`.rule-num${i}`);
+  const rect = element.getBoundingClientRect();
+  const xPosition = rect.left;
+  const yPosition = rect.top;
+  
+  if (rules[i].sub) {
+    html += `
+    <div class="rules-content-part rules-content-subpart" onclick="
+      toY(${yPosition+1700})
+    ">
+      <a class="rules-content-part-link rules-content-part-sublink">${rules[i].num} ${rules[i].name}</a>
+    </div>
+   `
+  } else {
+    html += `
+    <div class="rules-content-part" onclick="
+      toY(${yPosition+1700})
+    ">
+      <a class="rules-content-part-link">${rules[i].num} ${rules[i].name}</a>
+    </div>
+   `
+  }
+}
+
+document.querySelector('.rules-content').innerHTML = html;
